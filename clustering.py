b850aed4a23f3716554237714af029648471f7d9
giopina <giopina@sissa.it>
2016-10-08 10:34:10 +0200

    comments in clustering.py

--- clustering.py
+++ clustering.py
@@ -1,5 +1,18 @@
+### ---------------------------------------------
+### Example program that uses the UDPClust class 
+### to perform unsupervised density peak clustering
+### ---------------------------------------------
+### usage: python clustering.py input_file_name dimensionality output_file_name
+### 
+### NB: the dimensionality is a critical parameter and sometimes not trivial to estimate
+###     ask to Elena Facco, for further help on this
+### ---------------------------------------------
+### Written by Giovanni Pinamonti, SISSA, Trieste, 2016
+### ---------------------------------------------
+
 import numpy as np
 import sys
+
 #import cPickle as pickle
 import UDPClust as dp
 fname=sys.argv[1]
